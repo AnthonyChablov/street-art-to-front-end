999a0bbc417368752bbc03ffb991f4d21227601c
Anthony Chablov <achablov@myseneca.ca>
2022-12-23 19:26:02 -0500

    create file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './css/main.css';
 
 function App() {
   return (
---
+++ src/pages/StartApp/StartApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StartApp = () => {
+  return (
+    <div>StartApp</div>
+  )
+}
+
+export default StartApp
