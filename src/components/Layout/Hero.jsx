58869a90308f9d922df4a78aef10f40d3e3bae7a
Anthony Chablov <achablov@myseneca.ca>
2023-01-05 15:45:53 -0500

    make hero responsive

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   <section className="relative bg-heroImage bg-cover bg-center bg-no-repeat">
     <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/90 sm:to-white/25">
     </div>
-    <div className="relative mx-auto max-w-screen-xl px-4 py-32 h-screen  
+    <div className="relative mx-auto max-w-screen-xl px-4 py-80  h-screen  
       sm:px-6 md:px-8 
       md:items-center lg:flex lg:h-screen lg:items-center ">
       <div className="max-w-xl text-center sm:text-left">
